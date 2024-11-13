da7c4c8c7a6c6ba9aab199d8fd023cfc4888d577
Aravind Manoj <aravindmng47@gmail.com>
2024-11-13 12:42:54 +0530

    Added major updates

--- extension/src/Background.ts
+++ extension/src/Background.ts
@@ -1,10 +1,8 @@
 import Browser, { runtime } from 'webextension-polyfill';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const patternSingleQuotes = /\bify\('.*?'\)/;
-const patternDoubleQuotes = /\bify\(".*?"\)/;
-
 const GEMINI_API_KEY: any = Browser.storage.local.get("gemini_api_key");
+// Browser.storage.local.set({ })
 
 type TextMessage = {
     key: string;
@@ -17,14 +15,45 @@ type ToggleMessage = {
     isActive: boolean;
 }
 
-let data: string = "";
-let isActive: boolean = false;
-
+const data_history: any = [];
 const chat_history: any = [];
+const chat_history_bkp: any = [];
+
+var idx = 0;
+var data: string = "";
+var isActive: boolean = false;
+
+const patternSingleQuotes = /\bi\n*f\n*y\n*\(\s*'[\s\S]*?'\s*\)/;
+const patternDoubleQuotes = /\bi\n*f\n*y\n*\(\s*"[\s\S]*?"\s*\)/;
+const patternUndoSignal = /\bi\n*f\n*y\n*\(\s*\-\s*\)/;
+const patternRedoSignal = /\bi\n*f\n*y\n*\(\s*\+\s*\)/;
+
+const queryFormat = (char: string, c: number) => {
+    let index: number = 0;
+    if (data.search(patternSingleQuotes) != -1){
+        index = data.search(patternSingleQuotes);
+    }
+    if (data.search(patternDoubleQuotes) != -1){
+        index = data.search(patternDoubleQuotes);
+    }
+    if (data.search(patternUndoSignal) != -1){
+        index = data.search(patternUndoSignal);
+    }
+    if (data.search(patternRedoSignal) != -1){
+        index = data.search(patternRedoSignal);
+    }
+    let _data = data.substring(index);
+    let post_text_i = _data.split("i")[1].split("f")[0];
+    let post_text_f = _data.split("f")[1].split("y")[0];
+    let post_text_y = _data.split("y")[1].split("(")[0];
+    let pre_query = _data.split(`i${post_text_i}f${post_text_f}y${post_text_y}(`)[1].split(`${char}`)[0];
+    let post_query = _data.split(`i${post_text_i}f${post_text_f}y${post_text_y}(`)[1].split(`${char}`)[c].split(")")[0];
+    return [post_text_i, post_text_f, post_text_y, pre_query, post_query];
+}
 
 const apiRequest = async (text: string) => {
     try{
-        var prompt = "You are Alphify, an AI text assistant designed to provide helpful, informative, and accurate responses based on user input. Your goal is to assist users by offering relevant and clear information without asking unnecessary questions or answering your own queries.\n";
+        let prompt = "You are Alphify, an AI text assistant designed to provide helpful, informative, and accurate responses based on user input. Your goal is to assist users by offering relevant and clear information without asking unnecessary questions or answering your own queries.\n";
         const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
         chat_history.forEach((element: string[]) => {
@@ -32,7 +61,6 @@ const apiRequest = async (text: string) => {
         });
         prompt += `User: ${text}`;
         const output = await model.generateContent(prompt);
-        console.log(output);
         if (output.response){
             const response = output.response.text();
             chat_history.push([text, response])
@@ -40,7 +68,6 @@ const apiRequest = async (text: string) => {
         }
         return { success: false, message: "Something went wrong. Please try again." };
     }catch (e: any){
-        console.log(e);
         return e.message.includes("Resource has been exhausted") ? { success: false, message: "API quota exceeded. Please try again." } : { success: false, message: "API error occurred. Please try again." };
     }
 };
@@ -58,30 +85,59 @@ runtime.onMessage.addListener(async (message: unknown, sender, sendResponse) => 
     if ((message as TextMessage).type === 'execute') {
         data = (message as TextMessage).value;
         if (patternDoubleQuotes.test(data)){
-            let query = data.split('ify("')[1].split('"')[0].replace("\n", "");
+            const [i0, i1, i2, i3, i4] = queryFormat('"', 2);
+            let query = data.split(`i${i0}f${i1}y${i2}(`)[1].trim().split('"')[1].split('"')[0];
             if (query.length > 0){
-                const response: any = await apiRequest(query);
+                const response: any = await apiRequest(query.replace("\n", ""));
                 if (response.success){
-                    return { type: "textUpdate", key: `ify("${query}")`, value: response.message };
+                    data_history[idx] = (message as TextMessage).value.replace(`i${i0}f${i1}y${i2}(${i3}"${query}"${i4})`, "");
+                    data_history[++idx] = (message as TextMessage).value.replace(`i${i0}f${i1}y${i2}(${i3}"${query}"${i4})`, response.message);
+                    for (let i = data_history.length - 1; i > idx; i--) {
+                        data_history.pop();
+                    }
+                    return { type: "textUpdate", key: `i${i0}f${i1}y${i2}(${i3}"${query}"${i4})`, value: response.message };
                 }else{
-                    return { type:"error", key: `ify("${query}")`, value: response.message };
+                    return { type:"error", key: `i${i0}f${i1}y${i2}(${i3}"${query}"${i4})`, value: response.message };
                 }
             }
         }
         if (patternSingleQuotes.test(data)){
-            let query = data.split("ify('")[1].split("'")[0].replace("\n", "");
+            const [i0, i1, i2, i3, i4] = queryFormat("'", 2);
+            let query = data.split(`i${i0}f${i1}y${i2}(`)[1].trim().split("'")[1].split("'")[0];
+            data_history[data_history.length] = (message as TextMessage).value.replace(`i${i0}f${i1}y${i2}(${i3}'${query}'${i4})`, "");
+            idx++;
             if (query.length > 0){
-                const response: any = await apiRequest(query);
+                const response: any = await apiRequest(query.replace("\n", ""));
                 if (response.success){
-                    return { type: "textUpdate", key: `ify('${query}')`, value: response.message };
+                    data_history[data_history.length] = (message as TextMessage).value.replace(`i${i0}f${i1}y${i2}(${i3}'${query}'${i4})`, response.message);
+                    idx++;
+                    return { type: "textUpdate", key: `i${i0}f${i1}y${i2}(${i3}'${query}'${i4})`, value: response.message };
                 }else{
-                    return { type:"error", key: `ify('${query}')`, value: response.message };
+                    return { type:"error", key: `i${i0}f${i1}y${i2}(${i3}'${query}'${i4})`, value: response.message };
                 }
             }
+        }
+    }
+    if ((message as TextMessage).type === "undo"){
+        const [i0, i1, i2, i3, i4] = queryFormat("-", 1);
+        if (data_history[idx - 1] != undefined){
+            chat_history_bkp.push(chat_history.pop());
+            return { type: "undo", key: "", value: data_history[--idx] };
+        }else{
+            return { type: "error", key: `i${i0}f${i1}y${i2}(${i3}-${i4})`, value: "Operation can't be performed. No more undos available."};
+        }
+    }
+    if ((message as TextMessage).type === "redo"){
+        const [i0, i1, i2, i3, i4] = queryFormat("+", 1);
+        if (data_history[idx + 1] != undefined){
+            chat_history.push(chat_history_bkp.pop());
+            return { type: "redo", key: "", value: data_history[++idx] };
+        }else{
+            return { type: "error", key: `i${i0}f${i1}y${i2}(${i3}+${i4})`, value: "Operation can't be performed. No more redos available."};
         }
     }
     return false;
 });
 
-export { patternSingleQuotes, patternDoubleQuotes };
+export { patternSingleQuotes, patternDoubleQuotes, patternUndoSignal, patternRedoSignal };
 export type { TextMessage };
--- extension/src/Content.ts
+++ extension/src/Content.ts
@@ -1,5 +1,5 @@
 import Browser, { runtime } from 'webextension-polyfill';
-import { patternSingleQuotes, patternDoubleQuotes } from './Background';
+import { patternSingleQuotes, patternDoubleQuotes, patternUndoSignal, patternRedoSignal } from './Background';
 import type { TextMessage } from './Background';
 
 interface StorageResult {
@@ -21,43 +21,69 @@ const sendText = (event: Event) => {
 };
 
 const sendExecute = async (event: Event) => {
-    const text = (event.target as HTMLInputElement | HTMLTextAreaElement).value;
-    const element = document.createElement('div');
-    document.body.appendChild(element);
-    const original = (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor;
-    if (patternSingleQuotes.test(text) || patternDoubleQuotes.test(text) && (await Browser.storage.local.get('isActive') as StorageResult).isActive){
-        (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor = "rgb(134 76 206)";
-        element.textContent = "iFy is thinking...";
-        element.style.position = 'fixed';
-        element.style.top = '0';
-        element.style.right = '0';
-        element.style.borderRadius = '0 0 0 10px';
-        element.style.backgroundColor = 'rgb(134 76 206)';
-        element.style.padding = '12px 20px';
-        element.style.zIndex = '1000';
-        element.style.pointerEvents = 'none';
-        element.style.fontFamily = '"Helvetica Neue", Arial, sans-serif';
-        element.style.fontWeight = '600';
-        element.style.color = 'white';
-    }
-    runtime.sendMessage({ type: 'execute', key: "", value: text }).then((response: any) => {
-        (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor = original;
-        document.body.removeChild(element);
-        let cursorPos = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart;
-        if (response.type == "textUpdate"){
-            let beforeCursor = text.substring(0, cursorPos!);
-            let afterCursor = text.substring(cursorPos!);
-            (event.target as HTMLInputElement | HTMLTextAreaElement).value = (beforeCursor + afterCursor).replace((response as TextMessage).key, (response as TextMessage).value);
-            (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionEnd = text.indexOf((response as TextMessage).key) + (response as TextMessage).value.length;
+    if ((await Browser.storage.local.get('isActive') as StorageResult).isActive){
+        const text = (event.target as HTMLInputElement | HTMLTextAreaElement).value;
+        if (patternSingleQuotes.test(text) || patternDoubleQuotes.test(text)){
+            const element = document.createElement('div');
+            document.body.appendChild(element);
+            const original = (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor;
+            (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor = "rgb(134 76 206)";
+            element.textContent = "iFy is thinking...";
+            element.style.position = 'fixed';
+            element.style.top = '0';
+            element.style.right = '0';
+            element.style.borderRadius = '0 0 0 10px';
+            element.style.backgroundColor = 'rgb(134 76 206)';
+            element.style.padding = '12px 20px';
+            element.style.zIndex = '1000';
+            element.style.pointerEvents = 'none';
+            element.style.fontFamily = '"Helvetica Neue", Arial, sans-serif';
+            element.style.fontWeight = '600';
+            element.style.color = 'white';
+            runtime.sendMessage({ type: 'execute', key: "", value: text }).then((response: any) => {
+                (event.target as HTMLInputElement | HTMLTextAreaElement).style.outlineColor = original;
+                document.body.removeChild(element);
+                let cursorPos = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart;
+                let beforeCursor = text.substring(0, cursorPos!);
+                let afterCursor = text.substring(cursorPos!);
+                console.log(response);
+                if (response.type === "textUpdate"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (beforeCursor + afterCursor).replace((response as TextMessage).key, (response as TextMessage).value);
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionEnd = text.indexOf((response as TextMessage).key) + (response as TextMessage).value.length;
+                }
+                if (response.type === "error"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (beforeCursor + afterCursor).replace((response as TextMessage).key, "");
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionEnd = text.indexOf((response as TextMessage).key);
+                    alert(response.value);
+                }
+            });
         }
-        if (response.type == "error"){
-            let beforeCursor = text.substring(0, cursorPos!);
-            let afterCursor = text.substring(cursorPos!);
-            (event.target as HTMLInputElement | HTMLTextAreaElement).value = (beforeCursor + afterCursor).replace((response as TextMessage).key, "");
-            (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionEnd = text.indexOf((response as TextMessage).key);
-            alert(response.value);
+        if (patternUndoSignal.test(text)){
+            runtime.sendMessage({ type: 'undo', key: "", value: text }).then((response: any) => {
+                let cursorPos = (event.target as HTMLInputElement | HTMLTextAreaElement).selectionStart;
+                let beforeCursor = text.substring(0, cursorPos!);
+                let afterCursor = text.substring(cursorPos!);
+                if (response.type === "undo"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (response as TextMessage).value;
+                }
+                if (response.type === "error"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (beforeCursor + afterCursor).replace((response as TextMessage).key, "");
+                    alert(response.value);
+                }
+            });
         }
-    });
+        if (patternRedoSignal.test(text)){
+            runtime.sendMessage({ type: 'redo', key: "", value: text }).then((response: any) => {
+                if (response.type === "redo"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (response as TextMessage).value;
+                }
+                if (response.type === "error"){
+                    (event.target as HTMLInputElement | HTMLTextAreaElement).value = (event.target as HTMLInputElement | HTMLTextAreaElement).value.replace((response as TextMessage).key, "");
+                    alert(response.value);
+                }
+            });
+        }
+    }    
 }
 
 inputs.forEach(input => {
